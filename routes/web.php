f908a859a9a7169a4bfebaa5ef9fe175ee363280
Aoyu <liuaoyu2012@yeah.net>
2018-04-28 11:59:47 +0000

    Category topic list

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,5 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 //设置资源路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
-
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
