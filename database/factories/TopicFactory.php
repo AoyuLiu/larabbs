73724f518c2db6a5c5c66da8661a6e7d2a8e25b0
Aoyu <liuaoyu2012@yeah.net>
2018-04-28 08:02:55 +0000

    seed topics and users

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,18 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+
+    $sentence = $faker->sentence();
+    //随机获取一个月以内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+    //传参为生成时间不能超过的时间，创建时间应该比更新时间早！
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body'  => $faker->text(),
+        'excerpt'=> $sentence,
+        'created_at'=> $created_at,
+        'updated_at'=> $updated_at,
     ];
 });
