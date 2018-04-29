54a099ac96c8214c4af4a16f73d386e09f6e1b2e
Aoyu <liuaoyu2012@yeah.net>
2018-04-29 08:03:50 +0000

    User can create topic

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -4,3 +4,10 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+function make_excerpt($value, $length=200)
+{
+   $excerpt = trim(preg_replace('/\r\n|\r|\n+/','',strip_tags($value) ));
+   //辅助方法 见字符串
+   return str_limit($excerpt, $length);
+}
