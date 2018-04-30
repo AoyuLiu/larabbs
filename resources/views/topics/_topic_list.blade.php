0754346ad6423d354954fc3ac5f632f40af0545a
Aoyu <liuaoyu2012@yeah.net>
2018-04-30 14:47:30 +0000

    完成用户回复

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -12,17 +12,17 @@
                 <div class="media-body">
 
                     <div class="media-heading">
-                        <a href="{{ $topic->link() }}" title="{{ $topic->title }}">
+                        <a href="{{ route('topics.show', [$topic->id]) }}" title="{{ $topic->title }}">
                             {{ $topic->title }}
                         </a>
-                        <a class="pull-right" href="{{ $topic->link() }}" >
+                        <a class="pull-right" href="{{ route('topics.show', [$topic->id]) }}" >
                             <span class="badge"> {{ $topic->reply_count }} </span>
                         </a>
                     </div>
 
                     <div class="media-body meta">
 
-                        <a href="{{ route('categories.show',$topic->category->id) }}" title="{{ $topic->category->name }}">
+                        <a href="{{ route('categories.show', $topic->category->id) }}" title="{{ $topic->category->name }}">
                             <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
                              {{ $topic->category->name }}
                         </a>
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,5 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
+
+Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
