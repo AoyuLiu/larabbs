3d2512ce3bcdc67c81cba9ae5520903306930ba6
Aoyu <liuaoyu2015@gmail.com>
2018-05-09 09:54:58 +0000

    add jwt

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,14 @@
 <?php
-
 namespace App\Models;
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Auth;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Traits\LastActivedAtHelper;
     use Traits\ActiveUserHelper;
@@ -86,4 +87,25 @@ class User extends Authenticatable
 
        $this->attributes['avatar'] = $path;
    }
+
+
+   public function getJWTIdentifier()
+   {
+        return $this->getKey();
+   }
+
+   public function getJWTCustomClaims()
+   {
+        return [];
+   }
+
+
+
+
+
+
+
+
+
+
 }
