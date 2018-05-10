4732221e71b58cc7218ddb38d730018b68bd877f
Aoyu <liuaoyu2015@gmail.com>
2018-05-10 07:52:37 +0000

    topic update api

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -17,4 +17,13 @@ class TopicsController extends Controller
     	return $this->response->item($topic, new TopicTransformer())
     		->setStatusCode(201);
     }
+
+    public function update(TopicRequest $request, Topic $topic)
+    {   //用户授权策略
+    	$this->authorize('update', $topic);
+
+    	$topic->update($request->all());
+    	return $this->response->item($topic, new TopicTransformer());
+
+    }
 }
