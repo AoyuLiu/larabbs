90abf7fee99d13e19b652bd759e2b398ef125e3d
AoyuLiu <liuaoyu2012@yeah.net>
2018-05-13 07:17:46 +0000

    locale

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -20,7 +20,8 @@ class AuthorizationsController extends Controller
         $credentials['password'] = $request->password;
 
         if (!$token = Auth::guard('api')->attempt($credentials)) {
-            return $this->response->errorUnauthorized('用户名或密码错误');
+            // return $this->errorResponse(403,'您还没有通过认证',1003);
+            return $this->response->errorUnauthorized(trans('auth.failed'));
         }
 
         return $this->respondWithToken($token)->setStatusCode(201);
